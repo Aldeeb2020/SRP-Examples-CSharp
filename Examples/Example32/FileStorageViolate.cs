293b716d9c825c37cabdbe2883510b6f40bb5087
Aldeeb2020 <64950815+Aldeeb2020@users.noreply.github.com>
2025-06-02 10:10:57 +0300

    Update FileStorageViolate.cs

---
+++ Examples/Example32/FileStorageViolate.cs
@@ -0,0 +1,12 @@
+public class FileStorage
+{
+    public void SaveFile(string path)
+    {
+        Console.WriteLine($"Saving file to {path}.");
+    }
+
+    public void EncryptFile(string path)
+    {
+        Console.WriteLine($"Encrypting file at {path}.");
+    }
+}

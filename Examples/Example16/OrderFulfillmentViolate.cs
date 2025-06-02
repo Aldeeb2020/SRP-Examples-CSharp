22f4b349bc2457aad12b0e43f82fbd5c8895dd96
Aldeeb2020 <64950815+Aldeeb2020@users.noreply.github.com>
2025-06-02 09:27:57 +0300

    Update OrderFulfillmentViolate.cs

---
+++ Examples/Example16/OrderFulfillmentViolate.cs
@@ -0,0 +1,12 @@
+public class Order
+{
+    public void FulfillOrder()
+    {
+        Console.WriteLine("Fulfilling order.");
+    }
+
+    public void ShipOrder()
+    {
+        Console.WriteLine("Shipping order.");
+    }
+}

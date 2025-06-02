1b5ce3642472c59a44f5b56d18b3d5e47d8c927f
Aldeeb2020 <64950815+Aldeeb2020@users.noreply.github.com>
2025-06-02 09:26:42 +0300

    Update SupportAgentViolate.cs

---
+++ Examples/Example15/SupportAgentViolate.cs
@@ -0,0 +1,12 @@
+public class SupportAgent
+{
+    public void HandleTicket(string issue)
+    {
+        Console.WriteLine($"Handling ticket: {issue}.");
+    }
+
+    public void LogTicket(string issue)
+    {
+        Console.WriteLine($"Logging ticket: {issue}.");
+    }
+}

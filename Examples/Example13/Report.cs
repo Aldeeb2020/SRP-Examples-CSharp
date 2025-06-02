1bf6c2356a26d16cf7cfe0f469dbcec99589375c
Aldeeb2020 <64950815+Aldeeb2020@users.noreply.github.com>
2025-06-02 09:24:05 +0300

    Update Report.cs

---
+++ Examples/Example13/Report.cs
@@ -0,0 +1,15 @@
+public class ReportGenerator
+{
+    public void GenerateReport()
+    {
+        Console.WriteLine("Generating report.");
+    }
+}
+
+public class ReportFormatter
+{
+    public void FormatReport()
+    {
+        Console.WriteLine("Formatting report.");
+    }
+}
